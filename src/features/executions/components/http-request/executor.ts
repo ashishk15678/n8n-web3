2dfc7a17dd12d51dd4b718f93fbfdc3a51c497f4
Ashish Kumar <15678ashishk@gmail.com>
2025-11-08 12:07:03 +0530

    add templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,17 @@
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
+import handlebars from "handlebars";
+
+handlebars.registerHelper(
+  "json",
+  (context) => new handlebars.SafeString(JSON.stringify(context, null, 2)),
+);
 
 type HttpRequestTriggerData = {
-  variableName?: string;
-  endpoint?: string;
-  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE" | "OPTIONS";
+  variableName: string;
+  endpoint: string;
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE" | "OPTIONS";
   body?: string;
 };
 
@@ -15,19 +21,21 @@ export const HttpRequestTriggerExecutor: NodeExecutor<
   if (!data.variableName)
     throw new NonRetriableError("No variable name configured");
   if (!data.endpoint) throw new NonRetriableError("No endpoint configured");
+  if (!data.method) throw new NonRetriableError("No method configured");
 
   const result = await step.run("http-request", async () => {
-    const method = data.method || "GET";
-    const endpoint = data.endpoint!;
+    const method = data.method;
+    const endpoint = handlebars.compile(data.endpoint)(context);
     const options: KyOptions = { method };
     if (["POST", "PATCH", "PUT"].includes(method)) {
       if (data.body) {
-        options.body = data.body;
+        const resolved = handlebars.compile(data.body || "{}")(context);
+        JSON.parse(resolved);
+        options.body = resolved;
         options.headers = { "Content-Type": "application/json" };
       }
     }
     const response = await ky(endpoint, options);
-    // const response = await ky("https://codewithantonio.com");
     const responseType = response.headers.get("content-type");
     const responseData = responseType?.includes("application/json")
       ? await response.json()
@@ -44,7 +52,7 @@ export const HttpRequestTriggerExecutor: NodeExecutor<
     return {
       ...context,
 
-      [data.variableName as string]: responsePayload,
+      [data.variableName]: responsePayload,
     };
   });
 
